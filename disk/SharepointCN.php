1c7c6a54cded2f72dd0e3ced279c9a43066b23ac
SAOJSM <46414240+SAOJSM@users.noreply.github.com>
2021-08-25 13:21:26 +0800

    Update SharepointCN.php

--- disk/SharepointCN.php
+++ disk/SharepointCN.php
@@ -5,7 +5,7 @@ class SharepointCN extends Onedrive {
 
     function __construct($tag) {
         $this->disktag = $tag;
-        $this->redirect_uri = 'https://scfonedrive.github.io';
+        $this->redirect_uri = 'https://saojsm.github.io/Brian-Onemg';
         if (getConfig('client_id', $tag) && getConfig('client_secret', $tag)) {
             $this->client_id = getConfig('client_id', $tag);
             $this->client_secret = getConfig('client_secret', $tag);
